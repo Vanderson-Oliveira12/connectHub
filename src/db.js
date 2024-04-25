50d00f9bc5d3c68c2d17739e5a6772fb3e91a9d2
Vanderson-Oliveira12 <vanderson.doliveira14@gmail.com>
2024-04-25 14:03:30 -0300

    connect with db

--- src/db.js
+++ src/db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 
-const db = mongoose.connect("mongodb://127.0.0.1:27017/ConnectHub");
-
+const db = mongoose.connect("mongodb+srv://waleskagata14:<password>@cluster0.yw89hbe.mongodb.net/", {
+    auth: {
+        username: "waleskagata14",
+        password: "k2CObc7JGLdeJ8sz"
+    }
+});
+/* 177.125.97.198 */
 module.exports = db;
 
